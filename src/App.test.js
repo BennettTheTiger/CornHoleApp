14d2d12c40d396f765fcfc76cb7b9717681e1e17
BennettTheTiger <bas9386@g.rit.edu>
2019-06-28 23:03:29 -0400

    fixing failing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import rootReducer from './reducer/rootReducer';
+
+const store = createStore(rootReducer);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Provider store={store}><App /></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
